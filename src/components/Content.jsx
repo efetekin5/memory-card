8f43e9f6d702c791cd97c5a7e7e89567ffb874e1
efe <121581223+efetekin5@users.noreply.github.com>
2023-11-27 16:09:02 +0300

    created the grid with character images and names

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -8,7 +8,8 @@ export default function Content() {
         const getResponse = async () => {
             try {
                 const response = await fetch('https://thronesapi.com/api/v2/Characters');
-                const data = await response.json();    
+                const data = await response.json();  
+                setCharacterData(data);  
             } catch (error) {
                 console.log(error);
             }
@@ -20,7 +21,14 @@ export default function Content() {
     return(
         <div className='content'>
             <div className='grid'>
-                characterD
+                {characterData.slice(0, 8).map((character) => {
+                    return(
+                        <div className='character'>
+                            <img className='characterImg' src={character.imageUrl}></img>
+                            <p className='characterName'>{character.fullName}</p>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import '../styles/Header.css';
-import imdbLogo from '../assets/imdb.png'
+import gameOfThronesLogo from '../assets/gameOfThrones.png'
 
 export default function Header({score, bestScore}) {
     console.log('a')
     return(
         <div className="header">
-            <img className='imdbLogo' src={imdbLogo}></img>
+            <img className='gameOfThronesLogo' src={gameOfThronesLogo}></img>
             <h1 className='footerHeader'>Try Not To Click The Same Card Twice</h1>
             <div className='scores'>
                 <p className='score'>Current Score: {score}</p>
